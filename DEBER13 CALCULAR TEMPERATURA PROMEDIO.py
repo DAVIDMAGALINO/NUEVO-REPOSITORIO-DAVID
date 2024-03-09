c9a2da4badada5ec29529d1a5165a983c7575ee6
DAVID MAGARISCA <df.magariscal@uea.edu.ec>
2024-03-09 09:23:36 -0500

    deber13 temperatura promedio

--- DEBER13 CALCULAR TEMPERATURA PROMEDIO.py
+++ DEBER13 CALCULAR TEMPERATURA PROMEDIO.py
@@ -25,20 +25,3 @@ datos = {
 resultados = temperatura_promedio(datos)
 for ciudad, promedio in resultados.items():
     print(f"La temperatura promedio en {ciudad} es: {promedio}")
-temperaturas = [[[28, 30, 29, 27, 26, 25, 24],
-                 [27, 29, 28, 26, 25, 26, 23],
-                 [30, 31, 31, 30, 29, 28, 27]],
-                [[25, 26, 24, 23, 25, 28, 27],
-                 [28, 27, 29, 30, 31, 30, 29],
-                 [26, 27, 25, 24, 23, 22, 21]],
-                [[32, 31, 30, 29, 28, 27, 26],
-                 [26, 28, 30, 31, 32, 33, 34],
-                 [27, 28, 29, 30, 31, 32, 33]]]
-
-ciudades = ["Ciudad A", "Ciudad B", "Ciudad C"]
-dias_semana = ["Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"]
-
-for i, ciudad in enumerate(ciudades):
-    for j, semana in enumerate(temperaturas[i]):
-        promedio = sum(semana) / len(semana)
-        print(f"El promedio de temperaturas para la Ciudad {ciudad} en la semana {j+1} es: {promedio}")
